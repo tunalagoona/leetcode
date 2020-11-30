e91cdc521bcdfb5b9e459abc50981c6c98bae57a
TunaLagoona <maslova.wrk@gmail.com>
2020-11-30 13:13:59 +0000

    Add solution with better Time complexity

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -2,24 +2,43 @@ from typing import List
 
 
 class Solution:
+    # def remove_duplicates(self, nums: List[int]) -> int:
+    #     i = 1
+    #     l = len(nums)
+    #     while True:  # O(n^2) in the worst case
+    #         if i == l:
+    #             return l
+    #         if l <= 1:
+    #             return len(nums)
+    #         if nums[i - 1] == nums[i]:
+    #             del nums[i - 1]  # O(n)
+    #             l = len(nums)
+    #         else:
+    #             i += 1
+    #             if i == l:
+    #                 return l
+
     def remove_duplicates(self, nums: List[int]) -> int:
-        i = 1
         l = len(nums)
-        while True:
-            if i == l:
-                return l
-            if l <= 1:
-                return len(nums)
-            if nums[i - 1] == nums[i]:
-                del nums[i - 1]
-                l = len(nums)
-            else:
+        if l == 0:
+            return 0
+        i = 0
+        for j in range(1, l):  # O(n^2) in the worst case
+            if nums[j] != nums[i]:
                 i += 1
-                if i == l:
-                    return l
+                nums[i] = nums[j]
+            if j == l - 1:
+                k = i + 1
+                while True:
+                    if k < len(nums):
+                        del nums[k]
+                    else:
+                        break
+        print(nums)
+        return i + 1
 
 
 if __name__ == "__main__":
-    numbers = [1, 6, 7, 7, 7]
+    numbers = [1, 6, 7, 7, 7, 8]
     sol = Solution()
     print(sol.remove_duplicates(numbers))
