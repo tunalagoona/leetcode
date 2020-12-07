aa1c31512db10b28d5cf3d1957c059ca01ce6d50
TunaLagoona <maslova.wrk@gmail.com>
2020-12-07 16:44:18 +0000

    Add solution to "First bad version" problem

---
+++ first_bad_version.py
@@ -0,0 +1,49 @@
+"""
+Sorting and searching / easy
+"""
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+
+
+def isBadVersion(version):
+    if version % 4 == 0:
+        return True
+    else:
+        return False
+
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return self.first_bad(1, n, n)
+
+    def first_bad(self, low, high, first):
+        if low > high:
+            print('low > high')
+            first = low
+            return first
+        print(f'low = {low}, high = {high}, first bad = {first}')
+        m = low + (high - low + 1) // 2
+        print(f'm = {m}')
+        if isBadVersion(m):
+            if low == high:
+                first = low
+                return first
+            first = self.first_bad(low, m - 1, m)
+        else:
+            if low == high:
+                return first
+            first = self.first_bad(m + 1, high, first)
+        print(f'first bad = {first}')
+        return first
+
+
+if __name__ == '__main__':
+    num = 9
+    sol = Solution()
+    sol.firstBadVersion(num)
