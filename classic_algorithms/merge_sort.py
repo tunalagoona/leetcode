eda3952981d424fa5f7786caebe7b621d049d8c4
TunaLagoona <maslova.wrk@gmail.com>
2020-12-29 12:08:49 +0000

    Code refactoring

--- classic_algorithms/merge_sort.py
+++ classic_algorithms/merge_sort.py
@@ -1,13 +1,12 @@
 from typing import List, Optional
 
 
-def merge(a, b):
+def merge(a: List, b: List) -> List:
     a_len = len(a)
     b_len = len(b)
     res = [None] * (a_len + b_len)
-    i = 0
-    j = 0
-    k = 0
+    i, j, k = 0, 0, 0
+
     while i < a_len and j < b_len:
         if a[i] <= b[j]:
             res[k] = a[i]
@@ -28,7 +27,6 @@ def merge(a, b):
             res[k] = b[j]
             j += 1
             k += 1
-    print(f"res = {res}")
     return res
 
 
@@ -38,25 +36,14 @@ def merge_sort(arr, left: Optional[int] = None, right: Optional[int] = None) -> 
     if right is None:
         right = len(arr) - 1
 
-    print(f"merge_sort for {arr[left: right + 1]}")
-
     if left == right:
-        print("left = right")
         return arr[left : right + 1]
 
     if right - left > 0:
         mid = left + (right - left) // 2
         a = merge_sort(arr, left, mid)
         b = merge_sort(arr, mid + 1, right)
-        print(f"a and b = {a}, {b}")
         result = merge(a, b)
     else:
         return arr[left : right + 1]
     return result
-
-
-if __name__ == "__main__":
-    ar = [8, 4, 2, 15, 23, 4, 2, 49]
-    print(ar)
-    ar = merge_sort(ar)
-    print(ar)
